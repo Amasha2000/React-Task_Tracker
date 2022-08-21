ca63cab75711bb05909fad27b011eb46691e7e85
Amasha2000 <amashagalhenage@gmail.com>
2022-08-21 12:48:54 +0530

    Added Button and Task Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({title}) => {
+
+  const onClick=()=>{
+    
+  }
+
+
   return (
-    <header>
-      <h1 className='header'>{title}</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
+      {/* <Button color='blue' text='Hello2'/>
+      <Button color='red' text='Hello3'/> */}
     </header>
   )
 }
