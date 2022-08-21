fa21cb28e3e6dd3afd5e777d5254caa6d15e84f6
Amasha2000 <amashagalhenage@gmail.com>
2022-08-21 11:37:30 +0530

    Content Updated

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+
+  // const name='Mary';
+  // const x=true;
+
+  return (
+    <div className='App'>
+      {/* <h1>Hello From React</h1>
+      <h2>Hi {x ? 'Yes':'No'}</h2> */}
+      <Header/>
+    </div>
+  )
+}
+
+export default App
