ca700eb941ee9f40d20c80eafe042d0af37af0b8
Amasha2000 <amashagalhenage@gmail.com>
2022-08-21 12:24:28 +0530

    Added inline CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 const Header = ({title}) => {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   )
+}
+
+const headingStyle={
+  color:'red',
+  backgroundColor:'black'
 }
 
 Header.defaultProps={
